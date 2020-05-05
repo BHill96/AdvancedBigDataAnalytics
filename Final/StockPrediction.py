2672084b238720d804420793e4393852ce03abd0
Blake Hillier <blakehillier@mac.com>
2020-05-05 13:52:59 -0700

    Only uses stocks in timeframe

--- Final/StockPrediction.py
+++ Final/StockPrediction.py
@@ -246,7 +246,7 @@ data_test = data[data['DATE']>='2009-01-01']
 #Create test and training sets
 mae = []
 print('Testing Stocks...')
-for stock in stocks.columns[1:]:
+for stock in data.columns[7:]:
     modelColumns = np.append(macroFiles, stock)
     training = data_training[modelColumns]
     test = data_test[modelColumns]
