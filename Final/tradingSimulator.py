990d2a5cbe5c02b1ef66f255a02d58f10d314231
Blake Hillier <blakehillier@mac.com>
2020-05-04 21:46:33 -0700

    Added debuging statements

--- Final/tradingSimulator.py
+++ Final/tradingSimulator.py
@@ -213,6 +213,8 @@ def simulateMarket(T, dt, n, riskLevel, numRiskLevels, xlnetMetric, xlnetMetricT
         inpt, attMsk = XLNetFed.TextPrep(sentiment, MAX_LEN=MAX_LEN)
         model = XLNetFed.Train(inpt[:-1], attMsk[:-1], list(sentiment.Econ_Perf[:-1]), batch_size=batch,
                                epochs=epochs)
+        print('Trained')
+        print(type(inpt))
         currentText['Sentiment'] = XLNetFed.Predict(model, inpt, attMsk, batch)
         sentiment = turnDaily(usableStocks['DATE'], currentText[['Date','Sentiment']])
 
