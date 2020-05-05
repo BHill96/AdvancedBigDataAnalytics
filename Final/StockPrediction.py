5db03c6051e199c4caa648c9e4aeba44d2f081b7
Blake Hillier <blakehillier@mac.com>
2020-05-05 12:41:20 -0700

    Changes

--- Final/StockPrediction.py
+++ Final/StockPrediction.py
@@ -7,37 +7,45 @@ Created on Mon Mar 30 01:01:13 2020
 """
 
 from pandas import read_csv, read_excel, concat, merge_asof, DataFrame, to_datetime
+import pandas as pd
 import torch
 from torch.utils.data import TensorDataset, DataLoader, RandomSampler
 from XLNetFed import CalcSentiment, TextPrep
 from pytorch_transformers import XLNetForSequenceClassification, AdamW
 from tqdm import trange
+import tqdm
 import numpy as np
 from numpy import argmax
 from tensorflow.keras import Sequential, callbacks
 from tensorflow.keras.layers import Dense, LSTM, Dropout
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_error
 import matplotlib.pyplot as plt
 
 def turnDaily(stock, info):
+    stockDate = stock.columns[0]
+    infoDate = info.columns[0]
     daily = []
     colLabel = info.columns[1]
     i=len(info)-1
     j=len(stock)-1
     while j > -1 and i > -1:
-        if info['Date'][i] < stock['Date'][j]:
+        if info[infoDate][i] < stock[stockDate][j]:
+            #print('{2}: {0}<{1}'.format(info[infoDate][i], stock[stockDate][j], j))
             daily.append(info[colLabel][i])
             j = j-1
         else:
+            #print('{2}: {0}>{1}'.format(info[infoDate][i], stock[stockDate][j], i))
             i = i-1
     return daily[::-1]
 
 # Train XLNet
 EPOCHS = 10
-BATCH_SIZE = 24
+BATCH_SIZE = 28
+MAX_LEN = 128
 dataDr = 'Data/'
-textData = CalcSentiment(read_csv(dataDr+'FedTextData.csv', names=['Date','Text']), read_csv=read_csv(dir+'GDP.csv'))
-inpts, attMsks = TextPrep(textData)
+textData = CalcSentiment(read_csv(dataDr+'FedTextData.csv', names=['Date','Text']),
+                         read_csv=read_csv(dir+'liborfinal.csv'))
+inpts, attMsks = TextPrep(textData, MAX_LEN=MAX_LEN)
 
 # Turn data into torch tensors
 inpts = torch.tensor(inpts)
@@ -98,114 +106,167 @@ for batch in evalDataloader:
 
 textPred = DataFrame(textData.Date, columns=['Date'])
 textPred['Label'] = labels
+unh = pd.read_csv('Data/Stocks/UNH_data.csv')
+unh = pd.to_datetime(unh['Date'])
+unh = unh['Date'].dt.normalize()
+unh.drop(columns=unh.columns[1:], inplace=True)
 textDaily = turnDaily(unh, textPred)
 textDaily = DataFrame(textDaily)
 textDaily.dropna()
 textDaily.rename(columns = {'0':'EconPerf'}, inplace = True)
 
+
+
+def loadStocks():
+    #Read in files
+    # Should use the text file
+    s_and_p = ['MMM','ABT','ABBV','ACN','ATVI','AYI','ADBE','AMD','AAP','AES','AET',
+               'AMG','AFL','A','APD','AKAM','ALK','ALB','ARE','ALXN','ALGN','ALLE',
+               'AGN','ADS','LNT','ALL','GOOGL','GOOG','MO','AMZN','AEE','AAL','AEP',
+               'AXP','AIG','AMT','AWK','AMP','ABC','AME','AMGN','APH','ADI','ANDV',
+               'ANSS','ANTM','AON','AOS','APA','AIV','AAPL','AMAT','APTV','ADM',
+               'AJG','AIZ','T','ADSK','ADP','AZO','AVB','AVY','BLL','BAC','BK',
+               'BAX','BDX','BBY','BIIB','BLK','HRB','BA','BWA','BXP','BSX',
+               'BMY','AVGO','CHRW','CA','COG','CDNS','CPB','COF','CAH','CBOE',
+               'KMX','CCL','CAT','CBS','CNC','CNP','CTL','CERN','CF','SCHW',
+               'CHTR','CHK','CVX','CMG','CB','CHD','CI','XEC','CINF','CTAS','CSCO','C','CFG',
+               'CTXS','CLX','CME','CMS','KO','CTSH','CL','CMCSA','CMA','CAG','CXO','COP',
+               'ED','STZ','COO','GLW','COST','COTY','CCI','CSX','CMI','CVS','DHI',
+               'DHR','DRI','DVA','DE','DAL','XRAY','DVN','DLR','DFS','DISCA','DISCK','DISH',
+               'DG','DLTR','D','DOV','DTE','DRE','DUK','DXC','ETFC','EMN','ETN',
+               'EBAY','ECL','EIX','EW','EA','EMR','ETR','EVHC','EOG','EQT','EFX','EQIX','EQR',
+               'ESS','EL','ES','RE','EXC','EXPE','EXPD','ESRX','EXR','XOM','FFIV','FB','FAST',
+               'FRT','FDX','FIS','FITB','FE','FISV','FLIR','FLS','FLR','FMC','FL','F',
+	           'FBHS','BEN','FCX','GPS','GRMN','IT','GD','GE','GIS','GM','GPC','GILD',
+		       'GPN','GS','GT','GWW','HAL','HBI','HOG','HIG','HAS','HCA','HP','HSIC',
+		       'HSY','HES','HLT','HOLX','HD','HON','HRL','HST','HPQ','HUM','HBAN','HII',
+		       'IDXX','INFO','ITW','ILMN','INTC','ICE','IBM','INCY','IP','IPG','IFF','INTU',
+		       'ISRG','IVZ','IQV','IRM','JEC','JBHT','SJM','JNJ','JCI','JPM','JNPR','KSU','K','KEY',
+		       'KMB','KIM','KMI','KLAC','KSS','KR','LB','LH','LRCX','LEG','LEN',
+		       'LLY','LNC','LKQ','LMT','L','LOW','LYB','MTB','MAC','M','MRO','MPC','MAR','MMC','MLM',
+		       'MAS','MA','MAT','MKC','MCD','MCK','MDT','MRK','MET','MTD','MGM','MCHP','MU',
+		       'MSFT','MAA','MHK','TAP','MDLZ','MON','MNST','MCO','MS','MOS','MSI','MYL','NDAQ',
+		       'NOV','NAVI','NTAP','NFLX','NWL','NFX','NEM','NWSA','NWS','NEE','NLSN','NKE','NI',
+		       'NBL','JWN','NSC','NTRS','NOC','NCLH','NRG','NUE','NVDA','ORLY','OXY','OMC','OKE',
+		       'ORCL','PCAR','PKG','PH','PDCO','PAYX','PNR','PBCT','PEP','PKI','PRGO','PFE',
+		       'PCG','PM','PSX','PNW','PXD','PNC','RL','PPG','PPL','PFG','PG','PGR',
+		       'PLD','PRU','PEG','PSA','PHM','PVH','PWR','QCOM','DGX','RRC','RJF','RTN','O',
+		       'REG','REGN','RF','RSG','RMD','RHI','ROK','COL','ROP','ROST','RCL','CRM','SBAC',
+		       'SLB','SNI','STX','SEE','SRE','SHW','SIG','SPG','SWKS','SLG','SNA','SO','LUV',
+		       'SPGI','SWK','SBUX','STT','SRCL','SYK','STI','SYF','SNPS','SYY','TROW','TPR',
+		       'TGT','TEL','FTI','TXN','TXT','TMO','TIF','TWX','TJX','TSCO','TDG','TRV',
+		       'TRIP','TSN','UDR','ULTA','USB','UAA','UNP','UAL','UNH','UPS','URI',
+		       'UTX','UHS','UNM','VFC','VLO','VAR','VTR','VRSN','VRSK','VZ','VRTX','V','VNO',
+		       'VMC','WMT','WBA','DIS','WM','WAT','WEC','WFC','WDC','WU','WY','WHR','WMB',
+		       'WLTW','WYNN','XEL','XRX','XLNX','XL','XYL','YUM','ZBH','ZION','ZTS']
+
+    stocks = pd.read_csv('Data/Stocks/CI_data.csv').drop(['Open','High','Low','Close','Adj Close',
+                                                          'Volume', 'Name'], axis=1)
+    stocks.Date = pd.to_datetime(stocks['Date'])
+    stocks.Date = stocks['Date'].dt.normalize()
+    for tick in tqdm(s_and_p):
+        stock = pd.read_csv('Data/Stocks/'+tick+'_data.csv').drop(['Open','High','Low','Adj Close',
+                                                                   'Volume', 'Name'], axis=1)
+        # stock = stock[stock['Date'] >= '1994-01-01'] ??
+        apd = stock[stock['Date'] >= '1994-01-01'].copy()
+        stocks[tick] = apd['Close']
+    return stocks
+
 # Begin RNN LSTM
+stocks = loadStocks()
+
+"""
+Loads all the macro data from csv files and converts to daily timeframe.
+daily is the name of a stock
+ - Must be stored in Data/Stocks directory
+macro is the name of the macro data
+ - Must be stored in the Data directory
+* You do not need the file extension since we are using .csv only *
+"""
+def loadMacro(daily, macro):
+    dailyData = pd.read_csv('Data/Stocks/'+daily+'.csv')
+    dailyData.Date = pd.to_datetime(dailyData['Date'])
+    dailyData.Date = dailyData['Date'].dt.normalize()
+    dailyData.drop(columns=dailyData.columns[1:], inplace=True)
+    # print(dailyData)
+
+    for fileName in tqdm(macro):
+        #print('Loading '+fileName)
+        data = pd.read_csv('Data/'+fileName+'.csv').dropna()
+        data.DATE = pd.to_datetime(data['DATE'])
+        data.DATE = data['DATE'].dt.normalize()
+        # print(data)
+        # print(type(data.DATE[0]),type(data[data.columns[1]][0]))
+        dailyData[fileName] = turnDaily(dailyData, data)
+
+    return dailyData
+
 # Read in files
-gild = pd.read_csv('GILD.csv').drop(['Open','High','Low','Adj Close','Volume'], axis = 1)
-ci = pd.read_csv('CI.csv').drop(['Open','High','Low','Adj Close','Volume'], axis = 1)
-pfe = pd.read_csv('PFE.csv').drop(['Open','High','Low','Adj Close','Volume'], axis = 1)
-hum = pd.read_csv('HUM.csv').drop(['Open','High','Low','Adj Close','Volume'], axis = 1)
-Unh = pd.read_csv('UNH.csv').drop(['Open','High','Low','Adj Close','Volume'], axis = 1)
-tnx = pd.read_csv('TNX1.csv').drop(['Open','High','Low','Adj Close','Volume'], axis = 1)
-tnx.dropna(inplace = True)
-libor = pd.read_csv('liborfinal.csv')
-gdp = pd.read_csv('GDPC1.csv')
-cpi = pd.read_csv('CPIAUCSL.csv')
-inflation = pd.read_csv('MICH.csv')
-unemployment = pd.read_csv('UNRATENSA.csv')
+macroFiles = ['liborfinal','GDPC1','CPIAUCSL','MICH','UNRATENSA']
+macroDaily = loadMacro('UNH_data', macroFiles)
 
-#Create Empytlists for conversion to daily data
-gdpdaily = []
-cpidaily = []
-inflationdaily = []
-unemploymentdaily = []
+data = pd.merge(macroDaily, stocks, on='Date', how='outer').dropna(axis=1)
+data.sort_values(['Date'], inplace=True, axis=0, ascending=True)
+data.reset_index(inplace=True)
+data.drop('index', axis=1, inplace=True)
+data.rename(columns={'Date':'DATE'}, inplace=True)
 
+data_training = data[data['DATE']<'2009-01-01']
+data_test = data[data['DATE']>='2009-01-01']
 
-def dataclean(predictor, emptylist, filename):
-    i = 0
-    j = 0
-    while i < len(predictor['Date']):
-        if predictor['Date'][i] < predictor['DATE'][j]:
-            emptylist.append(predictor[filename][j])
-            i = i + 1
-        else:
-            j = j + 1
+def createModel(shape):
+    regressior = Sequential()
+    regressior.add(LSTM(units=256, activation='relu', return_sequences=True, input_shape=(shape[1], 1)))
+    regressior.add(Dropout(0.2))
+    regressior.add(LSTM(units=128, activation='relu', return_sequences=True))
+    regressior.add(Dropout(0.2))
+    regressior.add(LSTM(units=64, activation='relu', return_sequences=True))
+    regressior.add(Dropout(0.2))
+    regressior.add(LSTM(units=32, activation='relu'))
+    regressior.add(Dropout(0.2))
+    regressior.add(Dense(units=1))
+    return regressior
 
+def lstm(data, ticker, epochs=5, batch=64):
+    training.drop(training.tail(1).index, inplace=True)
 
-dataclean(gdp, gdpdaily, 'GDPC1')
-dataclean(cpi, cpidaily, 'CPIAUCSL')
-dataclean(inflation, inflationdaily, 'MICH')
-dataclean(unemployment, unemploymentdaily, 'UNRATENSA')
-        
-Unh['ci'] = ci['Close']
-Unh['gild'] = gild['Close']
-Unh['hum'] = hum['Close']
-Unh['pfe'] = pfe['Close']
-#Unh['tnx'] = tnx['Close']
-Unh['libor'] = libor[' value']
-Unh['cpi'] = cpidaily
-Unh['gdp'] = gdpdaily
-Unh['I'] = inflationdaily
-Unh['U'] = unemploymentdaily
-Unh['Txt'] = textDaily['EconPerf']
+    #Create X,Y Train Set
+    X = np.expand_dims(np.array(training), axis=2)
+    Y = np.array(data[ticker])[1:]
+    # Build network Structure
+    model = createModel(X.shape)
+
+    # Compile and train model
+    model.compile(optimizer='adam', loss='mean_squared_error')
+    # Verbose=0 makes it silent
+    model.fit(X, Y, epochs=epochs, batch_size=batch, verbose=0)
+    return model
 
 #Create test and training sets
-data_training1 = Unh[Unh['Date']<'2009-01-01'].copy()
-data_test = Unh[Unh['Date']>='2009-01-01'].copy()
+mae = []
+for stock in stocks.columns:
+    modelColumns = np.append(macroFiles, stock)
+    training = data_training[modelColumns]
+    test = data_test[modelColumns]
 
-data_training1 = data_training1.drop(['Date'], axis = 1)
-data_training = np.array(data_training1)
+    model = lstm(training, stock)
 
-#Create X,Y Train Set
-X_train = np.expand_dims(data_training, axis = 2)
-y_train = data_training1[['Close','ci','gild','hum','pfe']]
-y_train = np.array(y_train)
+    # Test Results
+    actual = np.array(test[stock])[1:]
+    test = test.drop(test.tail(1).index)
+    X = np.expand_dims(np.array(test), axis=2)
 
-#Create X test Set, y actual set
-data_test = data_test.drop(['Date'], axis = 1)
-data_test = np.array(data_test)
-X_test = np.expand_dims(data_test, axis = 2)
+    pred = model.predict(X)
+    predict = pred.tolist()
+    mae.append([stock, mean_absolute_error(predict,actual)])
+    print('Stock {0} MAE {1}'.format(stock, mae))
+
+mae = pd.DataFrame(mae)
+mae.to_csv('Data/finalModelMAE.csv')
 
 
-# Adjust shape as needed for data
-# Create RNN LSTM
-regressior = Sequential()
-
-regressior.add(LSTM(units = 256, activation = 'relu', return_sequences = True, input_shape = (X_train.shape[1], 1)))
-regressior.add(Dropout(0.2))
-
-regressior.add(LSTM(units = 128, activation = 'relu', return_sequences = True))
-regressior.add(Dropout(0.2))
-
-regressior.add(LSTM(units = 64, activation = 'relu', return_sequences = True))
-regressior.add(Dropout(0.2))
-
-regressior.add(LSTM(units = 32, activation = 'relu'))
-regressior.add(Dropout(0.2))
-
-regressior.add(Dense(units = 5))
-regressior.summary()
-
-regressior.compile(optimizer='adam', loss = 'mean_squared_error')
-# This prevents overfitting, only use during final testing and training
-callback = callbacks.EarlyStopping(monitor='loss', patience=5, min_delta=0.01)
-regressior.fit(X_train, y_train, epochs=50, batch_size=32, callbacks=[callback])
-
-# Test Results
-y_pred = regressior.predict(X_test)
-
-#Adjust actual stock value to have on same time scale on x axis
-unh1 = np.array(Unh['Close'][(5287-len(data_test)):5287])
-ci1 = np.array(Unh['ci'][(5287-len(data_test)):5287])
-gild1 = np.array(Unh['gild'][(5287-len(data_test)):5287])
-hum1 = np.array(Unh['hum'][(5287-len(data_test)):5287])
-pfe1 = np.array(Unh['pfe'][(5287-len(data_test)):5287])
-
-def prediction(predict, actual, stockname):    
+"""
+def prediction(predict, actual, stockname):
     plt.figure(figsize=(14,5))
     plt.plot(actual, color = 'red', label = 'Real' + stockname + 'Stock Price')
     plt.plot(predict, color = 'blue', label = 'Predicted' + stockname + 'Stock Price')
@@ -245,15 +306,10 @@ plt.savefig('StockPredXLNet.pdf')
 plt.savefig('StockPredXLNet.pgf', transparent=True)
 plt.show()
 
-#Create Covariance and Correlation matrices, Show heatmap
-mat = Unh.drop(columns = 'Date')
-mat.rename(columns = {'Close' : 'unh'}, inplace = True)
-sns.heatmap(mat.corr(), annot = True)
-
 # Print MSE for each stock and Residual Plot
 def metrics(predict, actual, stock):
     res = []
-    for i,j in zip(predict,actual):    
+    for i,j in zip(predict,actual):
         res.append(abs(i-j))
     plt.plot(res)
     plt.ylabel('Residual Squared')
@@ -270,4 +326,5 @@ metrics(y_pred[:,0],unh1, 'UNH')
 metrics(y_pred[:,1],ci1, 'CI')
 metrics(y_pred[:,2],gild1, 'GILD')
 metrics(y_pred[:,3],hum1, 'HUM')
-metrics(y_pred[:,4],pfe1, 'PFE')  
+metrics(y_pred[:,4],pfe1, 'PFE')
+"""
